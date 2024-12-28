e063c3d9c9e77717cb41734c951a21f4dd28c191
stone2324 <155638616+stone2324@users.noreply.github.com>
2024-12-28 18:20:24 +1100

    removed section

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,3 @@ from app.view import render_lifebook_page
 if __name__ == '__main__':
     st.set_page_config(page_title="Life Book", layout="wide")
     render_lifebook_page()
-
-# # st.title("🎈 My new app")
-# # st.write(
-# #     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-# # )
-
-# # st.write("Hello Worlds")
